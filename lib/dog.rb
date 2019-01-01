813bfab7ebc798905ea12eb8c506f5c9ed7ed9b9
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-01 20:41:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -11,8 +11,12 @@ class Dog
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
-    )
+    );
     SQL
+    DB[:conn].execute(sql)
+  end
+  def self.drop_table
+    sql = "DROP TABLE IF EXISTS dogs;"
     DB[:conn].execute(sql)
   end
 end
