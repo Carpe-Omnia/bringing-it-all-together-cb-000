a8197ad81c9e316e248a87f93a8e1f165249a64b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-01-01 20:40:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,14 @@ class Dog
     @breed = breed
     @id = id
   end
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+  end
 end
